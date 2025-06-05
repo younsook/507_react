074bf28a993c978fce1019731eae4a62d1d4c511
younsook <hodu0622@naver.com>
2025-06-05 13:09:42 +0900

    function GroupText

---
+++ src/01/GroupText.jsx
@@ -0,0 +1,8 @@
+function GroupText () {
+    return(
+        <div className="text-2xl bg-blue-400 text-white mb-1.5 p-2">
+            K-digital 2기
+        </div>
+    )
+}
+export default GroupText
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import GroupText from './01/GroupText';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let x=10;
   return (
     <>
-      <div>
+      <div className="flex bg-amber-200 justify-center">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,18 +16,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1 className='text-4xl text-amber-700 font-bold'>Vite + React</h1>
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <GroupText />
+      <GroupText />
+      <GroupText />
     </>
   )
 }
